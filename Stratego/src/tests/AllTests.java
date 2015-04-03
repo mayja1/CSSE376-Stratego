f480380b120083ac16a8155ccbb94ab515a53d7c
mayja1 <mayja1@rose-hulman.edu>
2015-04-03 08:01:57 -0400

    Added AllTests class to easily run all tests at once

---
+++ Stratego/src/tests/AllTests.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+		{
+			TestBombPiece.class,
+			TestClearPiece.class,
+			TestFlagPiece.class,
+			TestObstaclePiece.class,
+			TestPiece.class
+		})
+public class AllTests {
+	
+}
