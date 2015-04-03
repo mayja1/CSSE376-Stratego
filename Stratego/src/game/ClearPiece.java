6a17337df9ab6049be2d0ca84c019454f665e1f0
mayja1 <mayja1@rose-hulman.edu>
2015-04-03 09:47:29 -0400

    Clear Piece passes unit test

--- Stratego/src/game/ClearPiece.java
+++ Stratego/src/game/ClearPiece.java
@@ -3,23 +3,33 @@ package game;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 
+import javax.swing.border.LineBorder;
+
 public class ClearPiece extends AbstractPiece {
 	
 	private int index;
 	
 	public ClearPiece() {
 		addActionListener(this);
-		setBackground(Color.GREEN);
+		setBackground(Color.WHITE);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		observer.nonSelectedButtonPressed(location);
+		if(selected) {
+			observer.selectedButtonPressed(location);
+		}
 	}
 
 	@Override
 	public void setSelected(boolean selected) {
-		
+		if(selected) {
+			this.setBorder(new LineBorder(Color.RED, 5));
+		} else {
+			this.setBorder(new LineBorder(Color.GRAY, 1));
+		}
+
+		this.selected = selected;
 	}
 
 	@Override
