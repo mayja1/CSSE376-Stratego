a806049e16d6b682711d5a6c0724267722d94788
JJGayso <gaysojj@rose-hulman.edu>
2015-04-24 13:37:28 -0400

    Added test case of Marshall attacking, spy. (Spy should lose)

--- Stratego/src/tests/TestAttacking.java
+++ Stratego/src/tests/TestAttacking.java
@@ -134,5 +134,18 @@ public class TestAttacking {
 		assertEquals(null, s2.getLocation());
 	}
 	
+	@Test
+	public void TestMarshallAttackSpy() {
+		AbstractPiece s1 = new Soldier(9);
+		s1.setOwner(User.PLAYER1);
+		s1.setLocation(new Point(0, 1));
+		AbstractPiece s2 = new Soldier(0);
+		s2.setOwner(User.PLAYER2);
+		s2.setLocation(new Point(1, 1));
+		game.attack(s1, s2);
+		assertEquals(new Point(1, 1), s1.getLocation());
+		assertEquals(null, s2.getLocation());
+	}
+	
 
 }
