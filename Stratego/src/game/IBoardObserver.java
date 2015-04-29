467c915cf40ae2edfd95692735d1248b35fc072c
Skornage <millerna@rose-hulman.edu>
2015-04-29 18:13:06 -0400

    Forgot to add TestBoardObserver and IBoardObserver

---
+++ Stratego/src/game/IBoardObserver.java
@@ -0,0 +1,7 @@
+package game;
+
+public interface IBoardObserver {
+	
+	public void isDone(AbstractPiece[][] pieces);
+
+}
---
+++ Stratego/src/tests/TestBoardObserver.java
@@ -0,0 +1,37 @@
+package tests;
+
+import static org.junit.Assert.*;
+import game.AbstractPiece;
+import game.GameBoard;
+import game.PieceFactory;
+import game.PlayerBoard;
+import game.GameBoard.User;
+import game.SetupBoard;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBoardObserver {
+	
+	private PlayerBoard testBoard;
+	private AbstractPiece[][] pieces;
+	
+	@Before
+	public void setup() {
+		testBoard = new PlayerBoard(User.PLAYER1);
+		pieces = new AbstractPiece[10][4];
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 10; j++) {
+				pieces[j][i] = PieceFactory.createClearPiece();
+			}
+		}
+	}
+
+	@Test
+	public void testIsDone() {
+		testBoard.isDone(pieces);
+		assertFalse(testBoard.getBoard() instanceof SetupBoard);
+		assertTrue(testBoard.getBoard() instanceof GameBoard);
+	}
+
+}
