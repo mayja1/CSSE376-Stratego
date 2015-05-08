c235271d3ee9fca9839fc47d80e171bd95a1a948
mayja1 <mayja1@rose-hulman.edu>
2015-05-08 09:28:21 -0400

    Implemented methods in TurnObserver, test cases pass with method stub in GameBoard

--- Stratego/src/game/TurnObserver.java
+++ Stratego/src/game/TurnObserver.java
@@ -1,24 +1,37 @@
 package game;
 
+import java.awt.Point;
+import java.util.HashMap;
+
 import game.GameBoard.User;
 
 public class TurnObserver implements ITurnObserver {
 	
 	private Enum<User> turn = User.PLAYER1;
+	private HashMap<User, GameBoard> boardMap;
+	
+	public TurnObserver() {
+		boardMap = new HashMap<User, GameBoard>();
+	}
 	
 	@Override
-	public void endTurn() {
+	public void endTurn(Point p1, Point p2) {
 		if (turn == User.PLAYER1) {
 			turn = User.PLAYER2;
-			
 		} else {
 			turn = User.PLAYER1;
-		}		
+		}
+
+		boardMap.get(turn).updateBoard(p1, p2);
 	}
 
 	@Override
 	public boolean isTurn(Enum<User> user) {
 		return user.equals(turn);
+	}
+	
+	public void addBoard(User owner, GameBoard b) {
+		boardMap.put(owner, b);
 	}
 
 }
