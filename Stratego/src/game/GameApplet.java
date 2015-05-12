459fcc80d1d86b9fced48002f7fb5b0b39c9cb30
mayja1 <mayja1@rose-hulman.edu>
2015-05-12 10:18:46 -0400

    Added autoinitialization method

--- Stratego/src/game/GameApplet.java
+++ Stratego/src/game/GameApplet.java
@@ -3,6 +3,13 @@ import game.GameBoard.User;
 
 import javax.swing.JFrame;
 
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 public class GameApplet {
 
 	public static void main(String[] args) {
@@ -12,5 +19,36 @@ public class GameApplet {
 		player1Frame.setObserver(player2Frame);
 		player2Frame.setObserver(player1Frame);
 		player2Frame.setLocation(player1Frame.getX() + player1Frame.getWidth(), player2Frame.getY());
+		try {
+			autoInitialization();
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
 	}
+	
+	private static void autoInitialization() throws AWTException {
+		int delay = 60;
+		Robot r = new Robot();
+		Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
+		int count = 0;
+		for(int x = 50; x < screensize.width; x+=50) {
+			for(int y = 50; y < screensize.height; y+=50) {
+				if(r.getPixelColor(x, y).equals(Color.WHITE)) {
+					r.mouseMove(x, y);
+					r.delay(delay);
+					r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+					r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+					r.delay(delay);
+					r.keyPress(KeyEvent.VK_ENTER);
+					r.keyRelease(KeyEvent.VK_ENTER);
+					r.delay(delay);
+					count++;
+					if(count >=80) {
+						return;
+					}
+				}
+			}
+		}
+	}
+	
 }
